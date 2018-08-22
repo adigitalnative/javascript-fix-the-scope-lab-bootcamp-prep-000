abb3f3b7886c31ff6d9bce7d05e11efff4dfa1ae
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-22 20:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ function myAnimal() {
 
 function yourAnimal() {
   var animal = 'cat'
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
   return animal
 }
 
@@ -16,8 +13,6 @@ function add2(n) {
   const two = 2
   return n + two
 
-  // Feel free to move things around!
-  
 }
 
 var funkyFunction = function() {
